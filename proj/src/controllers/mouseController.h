b7439061bc1bcdf59d99b959256e7494bf2f94a7
arcinard <rebelo0912joao@outlook.com>
2024-05-26 13:19:58 +0100

    mouse fix menu

--- proj/src/controllers/mouseController.h
+++ proj/src/controllers/mouseController.h
@@ -6,6 +6,12 @@
 #include "Models/menu.h"
 #include "Models/mouse.h"
 
+#define MOUSE_SCALING_FACTOR 0.92
+#define SCREEN_WIDTH 1152
+#define SCREEN_HEIGHT 864
+#define CURSOR_WIDTH 50
+#define CURSOR_HEIGHT 50
+
 extern struct packet mouse_byte_packet;
 extern int mouse_errorHandling;
 
