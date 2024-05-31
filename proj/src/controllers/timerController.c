a73bacd7253326b24ab54bc8d4cffd1460dc7768
arcinard <rebelo0912joao@outlook.com>
2024-05-31 15:34:11 +0100

    fixed enemy movement

--- proj/src/controllers/timerController.c
+++ proj/src/controllers/timerController.c
@@ -4,7 +4,7 @@
 void timer_increment(){
     timerCounter++;
 }
-int handleDelayedEvents(tank *tank,EnemyTank *enemyTank MouseInfo* mouseInfo, Obstacle** obstacles, size_t numObstacles){
+int handleDelayedEvents(tank *tank,EnemyTank *enemyTank, MouseInfo* mouseInfo, Obstacle** obstacles, size_t numObstacles){
     // timer_increment();
     handleDelayedShooting(tank);
     if(!mouseInfo->canBuild){handleDelayedMouse(mouseInfo);}
