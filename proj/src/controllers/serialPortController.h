ebff3daca2182801b7b3438cfa46d1d2ffed8134
arcinard <rebelo0912joao@outlook.com>
2024-06-02 18:33:03 +0100

    doxygen doc serialPortController.h

--- proj/src/controllers/serialPortController.h
+++ proj/src/controllers/serialPortController.h
@@ -1,16 +1,31 @@
+/**
+ * @file serialPortController.h
+ * @brief Header file for serial port state management and interrupt handling.
+ */
+
 #ifndef __SERIAL_PORT__
 #define __SERIAL_PORT__
 
 #include "dev_interface/devices/serial_port.h"
 
+/**
+ * @brief Enumeration representing the different states of the serial port.
+ */
+typedef enum {
+    INIT,       /**< Initial state */
+    WAITING,    /**< Waiting state */
+    CONNECTED   /**< Connected state */
+} MSTATE;
 
-typedef enum{
-    INIT,
-    WAITING,
-    CONNECTED
-}MSTATE;
-
+/**
+ * @brief Handles the serial port interrupt.
+ *
+ * This function should be called when a serial port interrupt is generated.
+ * It processes the interrupt and performs necessary actions based on the 
+ * current state and received data.
+ *
+ * @return 0 upon success, non-zero otherwise.
+ */
 int handleSPInterrupt();
 
-#endif
-
+#endif /* __SERIAL_PORT__ */
